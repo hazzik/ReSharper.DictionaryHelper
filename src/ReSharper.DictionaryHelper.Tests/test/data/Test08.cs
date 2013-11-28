a4ac335d6cd452792726be38481d608537d5c601
Alexander Zaytsev <hazzik@gmail.com>
2013-11-29 11:26:48 +1300

    Trying to fix RSPL-5844

---
+++ src/ReSharper.DictionaryHelper.Tests/test/data/Test08.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace ReSharper.DictionaryHelper.Tests
+{
+    public class Test08
+    {
+        public string Method(IDictionary<int, string> dictionary, int key)
+        {
+            if (dictionary/*{caret}*/.ContainsKey(key))
+            {
+                var val = dictionary[key];
+                dictionary[key] = "a new value";
+                return dictionary[key];
+            }
+
+            return null;
+        }
+    }
+}
